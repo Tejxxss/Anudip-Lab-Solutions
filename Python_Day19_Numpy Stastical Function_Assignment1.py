e5c1ad09d5207762973ca39c50a693dc4c0f9ce0
Tejas Belote <146538954+Tejxxss@users.noreply.github.com>
2024-09-20 12:56:11 +0530

    Add files via upload

---
+++ Python_Day19_Numpy Stastical Function_Assignment1.py
@@ -0,0 +1,13 @@
+"""Compute the standard deviation of the NumPy array
+
+ Input: arr = [20, 2, 7, 1, 34]"""
+
+import numpy as np
+
+arr = np.array([20, 2, 7, 1, 34])
+std_deviation = np.std(arr)
+
+print(std_deviation)
+
+
+#Output:12.576167937809991
